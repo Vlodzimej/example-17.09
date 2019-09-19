187ba56ca7e8306154e4c0b2008b48ad18428e3c
Владимир Амелькин <amel-07@mail.ru>
2019-09-19 13:59:00 +0300

    Добавлен axios

--- components/App.jsx
+++ components/App.jsx
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
-
-import List from './List.jsx';
+import React, { Component } from "react";
+import axios from "axios";
+import List from "./List.jsx";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            list: []
-        };
+    this.state = {
+      list: []
+    };
 
-        console.log('constructor');
-    }
+    console.log("constructor");
+  }
 
-    componentDidMount() {
-        console.log('/list/count=3');
-        fetch('/list').then(res => res.json()).then(res => this.setState({ list: res }))
-    }
+  componentDidMount() {
+    console.log("/list/count=3");
+    axios
+      .get("/list", { params: { count: 3 } })
+      .then(res => this.setState({ list: res.data }));
+  }
 
-    render() {
-        console.log('state', this.state);
+  render() {
+    console.log("state", this.state);
 
-        const { list } = this.state;
+    const { list } = this.state;
 
-        return (
-            <div>
-                <List list={list}  />
-            </div>
-        );
-    }
+    return (
+      <div>
+        <List list={list} />
+      </div>
+    );
+  }
 }
 
 export default App;
